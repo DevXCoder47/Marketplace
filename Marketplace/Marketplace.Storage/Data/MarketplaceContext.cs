977e57644dbcedde316c6c6a65fff11813bc2c56
DevXCoder47 <nbelokrinitskiy@ukr.net>
2025-05-23 16:25:45 +0300

    Category and DTO added

--- Marketplace/Marketplace.Storage/Data/MarketplaceContext.cs
+++ Marketplace/Marketplace.Storage/Data/MarketplaceContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Marketplace.Core.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,6 @@ namespace Marketplace.Storage.Data
         {
 
         }
-
+        public DbSet<Category> Categories { get; set; }
     }
 }
