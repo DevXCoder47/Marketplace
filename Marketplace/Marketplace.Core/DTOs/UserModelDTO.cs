809f18cd0d91cebc40cdab6cd691c89c5bba694c
DevXCoder47 <nbelokrinitskiy@ukr.net>
2025-06-02 16:33:28 +0300

    UserModel added

---
+++ Marketplace/Marketplace.Core/DTOs/UserModelDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Core.DTOs
+{
+    public class UserModelDTO
+    {
+        public string Nickname { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Marketplace/Marketplace.Core/DTOs/UserSigneUpDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Core.DTOs
+{
+    public class UserSigneUpDTO
+    {
+        public string Nickname { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public string? Description { get; set; }
+    }
+}
