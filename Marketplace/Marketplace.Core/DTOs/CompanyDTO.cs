b9e77330a4e1f62472378b0d7b112ffb60a59bae
DevXCoder47 <nbelokrinitskiy@ukr.net>
2025-06-04 17:18:52 +0300

    furhter fixes

---
+++ Marketplace/Marketplace.Core/DTOs/CompanyDTO.cs
@@ -0,0 +1,20 @@
+﻿using Marketplace.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Core.DTOs
+{
+    public class CompanyDTO
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string TaxNumber { get; set; } = null!;
+        public string RegNumber { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public OnlineStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
