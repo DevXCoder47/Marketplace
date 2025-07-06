638e030c28e498b4cd2accdb079423afddc79abe
Scrolix1591 <scrolix36@gmail.com>
2025-07-06 12:48:51 +0200

    FIX

--- Marketplace/Marketplace.Core/Models/RefreshToken.cs
+++ Marketplace/Marketplace.Core/Models/RefreshToken.cs
@@ -1,9 +1,15 @@
-﻿using Marketplace.Core.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using Marketplace.Core.Interfaces;
 
 namespace Marketplace.Core.Models
 {
     public class RefreshToken : IEntity<string>
     {
+        public RefreshToken() 
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        [Key]
         public string Id { get; set; }
         public string Token { get; set; }
         public DateTime Expires { get; set; }
---
+++ Marketplace/Marketplace.Storage/Migrations/20250706104656_RefeshTokenUpdate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Marketplace.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefeshTokenUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "RefreshTokens",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "RefreshTokens",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
