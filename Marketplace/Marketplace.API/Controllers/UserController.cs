0415481c0ea462616ba8e612b093e4672f87252b
DevXCoder47 <nbelokrinitskiy@ukr.net>
2025-07-15 21:09:51 +0300

    UserController added

---
+++ Marketplace/Marketplace.API/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Marketplace.Core.DTOs;
+using Marketplace.Core.Interfaces;
+using Marketplace.Core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        private readonly IRepository _repository;
+
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("profile/id/{id}")]
+        public async Task<IActionResult> GetUserProfileById(string id)
+        {
+            try
+            {
+                var user = await _userManager.FindByIdAsync(id);
+                if (user == null)
+                    return NotFound($"User with ID '{id}' not found.");
+
+                var roles = await _userManager.GetRolesAsync(user);
+
+                var userDto = new UserDataDTO
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Roles = roles
+                };
+
+                return Ok(userDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
