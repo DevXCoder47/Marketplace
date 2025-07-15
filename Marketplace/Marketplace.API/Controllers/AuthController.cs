3fd859656f2de11d18166948676ec74e1d049b89
DevXCoder47 <nbelokrinitskiy@ukr.net>
2025-07-15 13:52:36 +0300

    endpoint added

--- Marketplace/Marketplace.API/Controllers/AuthController.cs
+++ Marketplace/Marketplace.API/Controllers/AuthController.cs
@@ -31,6 +31,34 @@ namespace Marketplace.API.Controllers
             _tokenService = tokenService;
         }
 
+        [Authorize]
+        [HttpGet("profile/id/{id}")]
+        public async Task<IActionResult> GetUserProfileById(string id)
+        {
+            try
+            {
+                var user = await _userManager.FindByIdAsync(id);
+                if (user == null)
+                    return NotFound($"User with ID '{id}' not found.");
+
+                var roles = await _userManager.GetRolesAsync(user);
+
+                var userDto = new UserDataDTO
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Roles = roles
+                };
+
+                return Ok(userDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
         [AllowAnonymous]
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterDTO dto)
