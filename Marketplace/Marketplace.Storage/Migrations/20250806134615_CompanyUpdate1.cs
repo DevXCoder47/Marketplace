9579a41547a24284f5f87f5381a7aed3fc851cc4
DevXCoder47 <nbelokrinitskiy@ukr.net>
2025-08-06 16:47:25 +0300

    Further fixes

---
+++ Marketplace/Marketplace.Storage/Migrations/20250806134615_CompanyUpdate1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marketplace.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompanyUpdate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CompanyPassword",
+                table: "Companies",
+                newName: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "CompanyEmail",
+                table: "Companies",
+                newName: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Companies",
+                newName: "CompanyPassword");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Companies",
+                newName: "CompanyEmail");
+        }
+    }
+}
